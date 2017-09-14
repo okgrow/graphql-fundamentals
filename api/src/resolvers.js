b4e00c0ca5874e11728745344b4f831cb280b631
xavcz <xavier.cazalot@gmail.com>
2017-09-14 15:47:15 -0400

    Exercise 9 #read-data-from-the-model
    [server]
    
    Goal: get real data for the `places` resolver!
    
    ### Instructions
    1. Create a resolver for the `places` query, reading data from the `Place` model
    ---
    ### Tips
    - Use `Place.all({})` in your `Query.places` resolver

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -2,29 +2,11 @@ import fetch from 'node-fetch';
 
 const resolvers = {
   Query: {
-    places: () => [
-      {
-        id: 'c92g93mthh8zhx6f3f58qhgbom2hy2',
-        name: 'Toronto',
-        visited: true,
-        latitude: 43.653908,
-        longitude: -79.384293,
-      },
-      {
-        id: 'z7akz06dau5iw95ykqqptidi3ccnqr',
-        name: 'Niagara Falls',
-        visited: false,
-        latitude: 43.092461,
-        longitude: -79.04715,
-      },
-      {
-        id: 'm3e8xlm1kox4jsjat87reqgj1jrn0x',
-        name: 'Grand Canyon',
-        visited: false,
-        latitude: 36.105976,
-        longitude: -112.094794,
-      },
-    ],
+    places: async (_, args, { Place }) => {
+      const places = await Place.all({});
+
+      return places;
+    },
     suggestions: async (_, args, context) => {
       // console.log(args);
       // console.log(context);
