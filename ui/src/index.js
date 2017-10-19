c92b302fd965f167a9c1278a06b7a4a7b5923acf
xavcz <xavier.cazalot@gmail.com>
2017-10-19 14:41:39 +0200

    Connect the client to the GraphQL API #apollo-client
    [UI]
    
    > This exercise is a walkthrough, follow along the live-coding
    
    ### Instructions
    - Create the Apollo Client and connect it to the GraphQL Server
    - Wire the Apollo Client to the React App
    ---
    ### Docs
    - [Set up Apollo Client](https://www.apollographql.com/docs/react/basics/setup.html)

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import App from './components/App';
-import defaultPlaces from './mocks';
 import './styles';
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App data={{ places: defaultPlaces }} />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
+
