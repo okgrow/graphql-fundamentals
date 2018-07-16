fe6a06bb3368b75e5ca8bd884216ff4f0b37d086
Robert Dickert <robert.dickert@gmail.com>
2018-07-16 17:04:36 -0600

    Write the createPlace mutation #add-to-a-collection
    [UI]
    > Make your changes in `ui/src/graphql/createPlaceMutation.js`
    
    When successful, you'll be able to new Places on the server.
    
    ### Instructions
    
    - This will be similar to the update mutation we just wrote but with variables.
    - Write the query in [Graphiql](http://localhost:8080/graphiql) as usual
    - You'll need to accept an input type as a variable - check the Docs in Graphiql as well as your previous work.
    - If you are creating a lot of places (and since we can't delete them), you can stop the api server and run `yarn reset-db` to remove all of your added places.

--- ui/src/graphql/createPlaceMutation.js
+++ ui/src/graphql/createPlaceMutation.js
@@ -1,1 +1,19 @@
-// import gql from 'graphql-tag';
+import gql from 'graphql-tag';
+
+export default gql`
+  mutation createPlace($input: CreatePlaceInput!) {
+    createPlace(input: $input) {
+      id
+      name
+      visited
+      location {
+        latitude
+        longitude
+        weather {
+          icon
+          temperature
+        }
+      }
+    }
+  }
+`;
