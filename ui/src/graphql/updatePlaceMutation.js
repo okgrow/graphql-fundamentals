b704440f4a0c1b629b1d47aa4f121404801b5e21
Robert Dickert <robert.dickert@gmail.com>
2018-07-16 17:07:56 -0600

    Write a mutation to set the `visited` status of a place #Client-Mutations
    [UI]
    
    > Make your changes in `ui/src/graphql/updatePlaceMutation.js`
    
    When successful, you'll be able to change the `visited` property of a Place on the server.
    
    ### Instructions
    - Write a mutation for `updatePlace($input: UpdatePlaceInput!)` in GraphiQL. It's exactly like writing a query, but the result will include a side-effect (changing visited status)
    - The mutation takes an input type, which is like a regular argument but may have more structure. Your object in Variables will need to follow that format (`{ "input": {"id": "5b510d52050690ed23bd751b", "visited": false }}`). You can inspect this in the graphiql Docs
    - The mutation receives a result just like a query does. Make sure you ask for `id`, `name`, and `visited`. There is a reason for this that we'll discuss in the next couple of exercises
    - To test, get a list of ids from getQueries (you can open a 2nd browser tab with Graphiql and switch between them).
    
    ---
    ### Reference
    
    - [GraphQL Mutations](https://graphql.org/learn/queries/#mutations)
    - [input types](https://graphql.org/learn/schema/#input-types)

--- ui/src/graphql/updatePlaceMutation.js
+++ ui/src/graphql/updatePlaceMutation.js
@@ -1,1 +1,11 @@
-// import gql from 'graphql-tag';
+import gql from 'graphql-tag';
+
+export default gql`
+  mutation updatePlace($input: UpdatePlaceInput!) {
+    updatePlace(input: $input) {
+      id
+      address
+      visited
+    }
+  }
+`;
