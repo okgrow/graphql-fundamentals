b0cbbaf1df578613ec6008206af81a89667720a4
Robert Dickert <robert.dickert@gmail.com>
2018-07-16 16:57:54 -0600

    Write a query in GraphiQL to load a suggested address from the GraphQL API #Client-Query-With-Variables
    [UI]
    
    When implemented, the input for adding a place will autocomplete with Google's single best match. E.g., entering "yosemite" will return:
    
    ```
    {
      "data": {
        "locationSuggestion": {
          "formattedAddress": "Yosemite National Park, California, USA"
        }
      }
    }
    ```
    
    - Use [graphiql](http://localhost:8080/graphiql) to build a query called `runSearch` that calls `locationSuggestion` query on the server. It should take a variable that you can set in the graphiql "Variables" pane.
    - When you have a working query, save it in `ui/src/graphql/runSearchQuery.js` (there is a file there waiting). Be sure to import `gql` so that you'll be able to use it in the next step.
    - the solution uses a variable name `$search`. This doesn't match the query api (`name`). This is so you can more easily see what name maps to what object. In real life, it's fine to give them the same name.
    - The return value is Google's best match for your search string. In this highly simplified example, it may just show an address, so cities and landmarks will give you better results. E.g., typing "yosemite" will show you Yosemite National Park, but "Home Depot" will probably give you a street address.
    
    ---
    ### Reference
    - [GraphQL language reference for arguments](http://graphql.org/learn/queries/#arguments)

--- ui/src/graphql/runSearchQuery.js
+++ ui/src/graphql/runSearchQuery.js
@@ -1,1 +1,9 @@
-// import gql from 'graphql-tag';
+import gql from 'graphql-tag';
+
+export default gql`
+  query runSearch($search: String) {
+    locationSuggestion(name: $search) {
+      formattedAddress
+    }
+  }
+`;
