88dffe29374c179f118ddb37a68dbaea7f21a792
Robert Dickert <robert.dickert@gmail.com>
2018-07-17 16:11:03 -0600

    Connect createPlace to the UI #add-to-a-collection
    [UI]
    
    > Make your changes in `ui/src/App/BucketList/AddPlace/AddPlace.js`
    
    When successful, adding a location in the UI will function as expected.
    
    ### Instructions
    
    - Use a Mutation component to provide the mutation function to the Input component.
    - The function that you provide to the `addPlace` prop on the Input will receive the value of the input field, which corresponds to the `address` required by the mutation. You will need to pass that in your call as variables.
    - Unlike our update function, this mutation will not automatically update our UI. For now add a refetchQueries prop to the Mutation:
    
    ```
    refetchQueries={['getPlaces']}
    ```
    
    This will cause the getPlaces query to refetch when the mutation is complete. We will improve upon this in the next step.

--- ui/src/App/BucketList/AddPlace/AddPlace.js
+++ ui/src/App/BucketList/AddPlace/AddPlace.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
 
+import createPlaceMutation from '../../../graphql/createPlaceMutation';
 import Input from './Input';
 
-const AddPlace = () => {
-  return <Input addPlace={() => {}} />;
-};
+const AddPlace = () => (
+  <Mutation mutation={createPlaceMutation} refetchQueries={['getPlaces']}>
+    {addPlace => {
+      const addPlaceWithAddress = address =>
+        addPlace({
+          variables: { input: { address } },
+        });
+      return <Input addPlace={addPlaceWithAddress} />;
+    }}
+  </Mutation>
+);
 
 export default AddPlace;
