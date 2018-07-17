2eadc8130e9412e93ff252c1b81b1adb6aababc6
Robert Dickert <robert.dickert@gmail.com>
2018-07-17 16:12:29 -0600

    Update a mutation result directly without requerying #add-to-a-collection
    [UI]
    
    The behavior of the app will remain the same, but you will eliminate one network request. You can view this in the network tab of the dev tools.
    
    > Make your changes in `ui/src/App/BucketList/AddPlace/AddPlace.js`
    
    ### Instructions
    
    - Use an `update` function (Mutation props) to directly update getPlaces
    - You will need to import `getPlaces` from the `graphql/` directory
    - Use `cache.readQuery()` and `cache.writeQuery()` to directly update the cache. Follow the example `update` function in the reference link below
    - If you get lost, you're not alone! This is a challenging activity. Try logging values in your update function to see what's going on.
    ---
    ### Reference
     - [Apollo Mutation update function](https://www.apollographql.com/docs/react/essentials/mutations.html#update)
    
    - NOTE: we had to directly read and write a specific query because although a query result can have its current members updated automatically, there is not innate way for Apollo to know that the members of a query have changed, so you'll always have to do this for create and delete, but generally not for update.

--- ui/src/App/BucketList/AddPlace/AddPlace.js
+++ ui/src/App/BucketList/AddPlace/AddPlace.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Mutation } from 'react-apollo';
 
 import createPlaceMutation from '../../../graphql/createPlaceMutation';
+import getPlacesQuery from '../../../graphql/getPlacesQuery';
+
 import Input from './Input';
 import styled from '../../../../node_modules/styled-components';
 
 const AddPlace = () => (
-  <Mutation mutation={createPlaceMutation} refetchQueries={['getPlaces']}>
+  <Mutation
+    mutation={createPlaceMutation}
+    update={(cache, { data: { createPlace } }) => {
+      console.log('updating', createPlace);
+      const { places } = cache.readQuery({
+        query: getPlacesQuery,
+      });
+      cache.writeQuery({
+        query: getPlacesQuery,
+        data: { places: places.concat([createPlace]) },
+      });
+    }}
+  >
     {addPlace => {
       const addPlaceWithAddress = address =>
         addPlace({
