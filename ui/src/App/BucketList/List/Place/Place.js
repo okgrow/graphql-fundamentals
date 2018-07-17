50f393719aa5b10848edff8b551c9f2b2b36aee5
Robert Dickert <robert.dickert@gmail.com>
2018-07-17 16:05:51 -0600

    Connect the updatePlace mutation to the UI #Client-Mutations
    [UI]
    
    > Make your changes in `ui/src/App/BucketList/List/Place/Place.js`
    
    When successful, clicking the checkbox next to a Place will change its status on the server (and the change will persist between when the app is refreshed).
    
    - Use a `Mutation` component to provide your mutation to `PlaceView`'s `toggleVisited` prop.
    - You'll need to set up a function to call the mutation correctly with variables in order to toggle `visited`. Remember you have access to `place` from props (you can console.log if you need to see what's available).
    - The variables you call can be modeled from what you did in graphiql in the last step
    - Add the variables when you call the mutation, not to the Mutation props.
    - If you want to prevent react warnings, you can set a `key` on the outermost component (probably Mutation but that's up to you).
    - bonus activity: remove `id` from your mutation result (in graphql/updatePlaceMutation. Now try toggling the visited status. What happens? Can you guess what's going on?
    ---
     ### Reference
    
    - [Apollo Mutation](https://www.apollographql.com/docs/react/essentials/mutations.html#basic)

--- ui/src/App/BucketList/List/Place/Place.js
+++ ui/src/App/BucketList/List/Place/Place.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
 
-// import updatePlaceMutation from '../../../../graphql/updatePlaceMutation';
+import updatePlaceMutation from '../../../../graphql/updatePlaceMutation';
 import PlaceView from './PlaceView';
 
-const Place = ({ place }) => {
-  return <PlaceView place={place} toggleVisited={() => {}} />;
-};
+const Place = ({ place }) => (
+  <Mutation key={place.id} mutation={updatePlaceMutation}>
+    {updatePlace => {
+      const toggleVisited = id => {
+        updatePlace({
+          variables: {
+            input: { id, visited: !place.visited },
+          },
+        });
+      };
+      return <PlaceView place={place} toggleVisited={toggleVisited} />;
+    }}
+  </Mutation>
+);
 
 export default Place;
