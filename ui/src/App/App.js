2687609dcac714b75bab3c074fbf277b0bead499
Robert Dickert <robert.dickert@gmail.com>
2018-07-18 10:05:22 -0600

    Handle loading and errors #apollo-client
    [UI]
    
    > Make your changes in `ui/src/App/App.js`
    
    Import the `Loading` and `Error` components (both `App/`) and return them instead of the main app when appropriate. See the [Apollo Query component](https://www.apollographql.com/docs/react/essentials/queries.html#basic) documentation for an example of how to do this.
    
    * Loading should show an animation
    * You can simulate an error simply by overriding the value of the error value returned by the Query component
    * For review: What is the benefit of handling errors here instead of passing them on in props?

--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -4,11 +4,16 @@ import { Query } from 'react-apollo';
 import Routes from './Routes';
 import getPlacesQuery from '../graphql/getPlacesQuery';
 
+import Loading from './Loading';
+import Error from './Error';
+
 const App = () => {
   return (
     <Query query={getPlacesQuery}>
       {graphqlResponse => {
-        const { data } = graphqlResponse;
+        const { data, loading, error } = graphqlResponse;
+        if (loading) return <Loading />;
+        if (error) return <Error />;
         return <Routes places={data.places} />;
       }}
     </Query>
