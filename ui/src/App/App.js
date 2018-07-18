c465c5c6efd841e6485f01345c43a279869daeb6
Robert Dickert <robert.dickert@gmail.com>
2018-07-18 10:04:45 -0600

    Replace the mocked places on the client with a live query to `getPlaces` #apollo-client
    [UI]
    
    > Make your changes in `ui/src/App/App.js`
    
    1. uncomment out the import statement to get access to the query you wrote.
    2. Use Apollo's `Query` component to run the query, and replace `mockPlaces` with data from the query.
    3. The `places` prop expects an array of objects similar to `mockPlaces.js` (also in the `App/` directory). Make sure you pass the only the array, and not the entire query result.
    
    ---
    ###Reference
    
    - [Queries with React Apollo](http://dev.apollodata.com/react/queries.html)

--- ui/src/App/App.js
+++ ui/src/App/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Query } from 'react-apollo';
 
 import Routes from './Routes';
-// import getPlacesQuery from '../graphql/getPlacesQuery';
-
-// delete mockPlaces and replace with a query result
-import mockPlaces from './mockPlaces';
+import getPlacesQuery from '../graphql/getPlacesQuery';
 
 const App = () => {
-  return <Routes places={mockPlaces} />;
+  return (
+    <Query query={getPlacesQuery}>
+      {graphqlResponse => {
+        const { data } = graphqlResponse;
+        return <Routes places={data.places} />;
+      }}
+    </Query>
+  );
 };
 
 export default App;
